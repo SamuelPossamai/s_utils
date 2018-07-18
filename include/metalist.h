d294a3598b0899eef36b8d09bb237aacec40992c
SamuelPossamai <possamai.sam@gmail.com>
2018-07-18 14:11:53 -0300

    Metalist added

---
+++ include/metalist.h
@@ -0,0 +1,55 @@
+
+#include <cstddef>
+
+template<typename... Args>
+class MetaList {
+    
+    template <std::size_t N, typename T, typename... MethodArgs> struct _get;
+    template <std::size_t N, template <typename> typename T> struct _apply_aux;
+    
+public:
+    
+    static constexpr std::size_t size() { return sizeof...(Args); }
+    
+    template <std::size_t N>
+    struct get {
+        
+        using Result = typename _get<N, Args...>::Result;
+    };
+    
+    template <template <typename> typename T>
+    static constexpr void applyToAll() {
+        
+        _apply_aux<0, T>::_apply_to_all();
+    }
+    
+private:
+    
+    template <std::size_t N, typename T, typename... MethodArgs>
+    struct _get {
+        
+        using Result = typename _get<N-1, MethodArgs...>::Result;
+    };
+    
+    template <typename T, typename... MethodArgs>
+    struct _get <0, T, MethodArgs...> {
+        
+        using Result = T;
+    };
+    
+    template <std::size_t N, template <typename> typename T>
+    struct _apply_aux {
+    
+        static constexpr void _apply_to_all() {
+            
+            T<typename get<N>::Result >();
+            _apply_aux<N+1, T>::_apply_to_all();
+        }
+    };
+    
+    template <template <typename> typename T> 
+    struct _apply_aux <size(), T> {
+        
+        static constexpr void _apply_to_all() {}  
+    };
+};
